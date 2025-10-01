e6fa53091a9f7f8fb25d05ab6e84c64a0852a8fd
ruaridhg <ruaridhg@users.noreply.github.com>
2025-10-01 10:43:27 +0100

    Simple test for checking datatype loaded correctly + info for accessing other useful variables

--- python/tests/test_server.py
+++ python/tests/test_server.py
@@ -10,4 +10,15 @@ def test_mrdPlugin_installed(xnat_uri):
         print(session.plugins)
         plugin = [plugin for plugin in session.plugins if plugin == "mrdPlugin"]
         assert plugin[0] == "mrdPlugin"
+        session.plugins.data[
+            "mrdPlugin"
+        ].version  # unknown vars() .name XNAT 1.8 ISMRMRD plugin
         print(xnat_uri)
+        inspector = xnat.inspect.Inspect(session)
+        mrddatatype = [
+            datatype
+            for datatype in inspector.datatypes()
+            if datatype == "mrd:mrdScanData"
+        ]
+        assert mrddatatype[0] == "mrd:mrdScanData"
+        inspector.datafields("mrdScanData")
